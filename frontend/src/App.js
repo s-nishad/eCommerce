fd8217e4d85cd59efc3ce73c7048bfe6fe886949
s-nishad <shohanurnishad@gmail.com>
2022-03-13 02:32:30 +0600

    Create React App Day_01

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">eCommerce</a>
+      </header>
+      <main>list Product</main>
+    </div>
+  );
+}
+
+export default App;
