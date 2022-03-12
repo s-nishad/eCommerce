f5b44fe3a91d63411b4578416cf5d4d29778bf03
s-nishad <shohanurnishad@gmail.com>
2022-03-13 04:11:05 +0600

    Day 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,31 @@
-//import './App.css';
+import p_data from './products_data';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a href="/">eCommerce</a>
+        <a href="/">E❤️CLick</a>
       </header>
-      <main>list Product</main>
+      <main>
+        <h1>Featured Products</h1>
+
+        <div className="products">
+          {p_data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+              <a href={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+                </a>
+                <p><strong>${product.price}</strong></p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/products_data.js
@@ -0,0 +1,49 @@
+const p_data = {
+  products: [
+    {
+      name: 'Nije Slim shirt',
+      slug: 'nike-slim-shirt',
+      catagory: 'Shirt',
+      image: '/images/p1.jpg',
+      price: '420',
+      rating: 4.5,
+      numReview: '',
+      description: 'High quality cotton',
+    },
+
+    {
+      name: 'Easy Slim shirt',
+      slug: 'easy-slim-shirt',
+      catagory: 'Shirt',
+      image: '/images/p2.jpg',
+      price: '120',
+      rating: 4.5,
+      numReview: '',
+      description: 'High quality cotton',
+    },
+
+    {
+      name: 'Clean Slim Pant',
+      slug: 'clean-slim-pant',
+      catagory: 'Shirt',
+      image: '/images/p3.jpg',
+      price: '1020',
+      rating: 4.5,
+      numReview: '',
+      description: 'High quality cotton',
+    },
+
+    {
+      name: 'Twinkle nice pant',
+      slug: 'twinkle-nice-pant',
+      catagory: 'Shirt',
+      image: '/images/p4.jpg',
+      price: '820',
+      rating: 4.5,
+      numReview: '',
+      description: 'High quality cotton',
+    }
+  ],
+};
+
+export default p_data;
